bcef1233e4401db9491501e595bcc5d44601b480
Arnout Kazemier <info@3rd-Eden.com>
2010-05-04 22:11:30 +0200

    Added execution time to testcases, Added first draft of the test_browser

--- test_browser.js
+++ test_browser.js
@@ -14,14 +14,14 @@ fs.readFile( './test/user_agent_data.csv', function( err, result ){
 
 	while( i-- ){
 		(function( ua, res ){
-			var agent = useragent.parser( ua.toString().replace( /\"/g, "" ) );
+			var agent = useragent.browser( ua.toString().replace( /\"/g, "" ) );
 
-			if( !agent || !agent.pretty() || !agent.match ){
+			/*if( !agent || !agent.pretty() || !agent.match ){
 				failed[ failed.length ] = Array.prototype.concat.call( arguments, [] ).join(",");
 				sys.puts( JSON.stringify( agent ) );
 				sys.puts( agent.pretty() )
 				sys.puts( ua  + "\n");
-			}
+			}*/
 
 		}( results[i].split( "," ) ))
 	};
