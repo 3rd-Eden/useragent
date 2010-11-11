9f6ca140967609d448f81991173555ca2a8efbc1
Arnout Kazemier <info@3rd-Eden.com>
2010-11-11 19:53:59 +0100

    updated the example to make the results more clear, added prettyOs and updated the osParser to use a UA based approach for parsing

--- example.js
+++ example.js
@@ -1,25 +1,35 @@
-var sys = require( 'sys' ),
-   	http = require( 'http' ),
-	userAgent = require( './useragent' );
-	
-http.createServer(
-	function( request, response ){
-		// creates a parsed down userAgent Object
-  		var ua_obj = userAgent.parser( request.headers['user-agent'] ),
-		
-		// the common know JS library ua parser, which makes it easier to target the most common browsers
-			is = userAgent.browser( request.headers['user-agent'] ),
-			content = "";
-		
-		content += is.webkit ? "Hello Webkit" : is.firefox ? "Hello Firefox" : "Hello Other";
-		content += "<br />is:" + JSON.stringify( is );
-		content += "<br />ua_obj:" + JSON.stringify( ua_obj );
-		
-		response.writeHead( 200, {'Content-Type': 'text/html'});
-		response.end(  content );
- 
-	}
-).listen( 8000 );
+var http = require( 'http' ),
+		userAgent = require( './useragent' );
 
-// zomg, where are we located
-sys.puts( 'Server running at http://127.0.0.1:8000/' );
+http.createServer(function( request, response ){
+	
+	// creates a parsed down userAgent Object
+	var userAgentString = request.headers['user-agent'],
+			ua_obj = userAgent.parser( userAgentString ),
+			is = userAgent.browser( userAgentString ),
+			content = "";
+	
+	// generate some body
+	content += "<h1>{ua}</h1>"
+	content += "<dl>";
+		content += "<dt>userAgent.is</dt><dd>{is}</dd>";
+		content += "<dt>userAgent.parser</dt><dd>{ua_obj}</dd>";
+		content += "<dt>userAgent response.pretty</dt><dd>{pretty}</dd>";
+		content += "<dt>userAgent response.prettyOs</dt><dd>{prettyOs}</dd>";
+	content += "</dl>";
+	
+	// write out the response
+	response.writeHead( 200, {'Content-Type': 'text/html'});
+	response.end(
+		// easy variable replacement
+		content.replace( "{ua}", userAgentString )
+					 .replace( "{ua_obj}", JSON.stringify( ua_obj ) )
+					 .replace( "{is}", JSON.stringify( is ) )
+					 .replace( "{pretty}", ua_obj.pretty() )
+					 .replace( "{prettyOs}", ua_obj.prettyOs() )
+	);
+
+}).listen( 8000 );
+
+// Display connection information
+console.log( 'Server running at http://127.0.0.1:8000/' );
--- useragent.js
+++ useragent.js
@@ -20,7 +20,7 @@ exports.browser = function browser( ua ){
 exports.parser = function parser( ua, js_ua ){
 	var V1 = V2 = V3 = family = false,
 		i = 0, length = browsers.length,
-		ua_obj, sequence;
+		os, ua_obj, sequence;
 	
 	// check we can parse down the ua based the parse object
 	function parse( parser_obj ){
@@ -65,7 +65,7 @@ exports.parser = function parser( ua, js_ua ){
 	};
 	
 	// create a pretty user agent string from the ua object
-	function pretty( user_agent ){
+	function pretty( user_agent, os ){
 		// use a supplied ua obj, or the buildin
 		var ua = user_agent ? user_agent : ua_obj
 			format = "$0";
@@ -85,11 +85,16 @@ exports.parser = function parser( ua, js_ua ){
 			}
 		}
 		
+		// operating system
+		if( ua.os && os )
+			format = format + " / $4";
+		
 		// replace placeholder with actual content
 		return format.replace( "$0", ua.family )
 					 .replace( "$1", ua.V1 )
 					 .replace( "$2", ua.V2 )
-					 .replace( "$3", ua.V3 );
+					 .replace( "$3", ua.V3 )
+					 .replace( "$4", ua.os )
 	};
 	
 	// the actual finding of the correct user agent group.
@@ -105,14 +110,12 @@ exports.parser = function parser( ua, js_ua ){
 	if( js_ua && ua.indexOf( "chromeframe" ) != -1 )
 		ua_obj.family = "Chrome Frame(" + ua_obj.family + " " + ua_obj.V1 + ")";
 
-	// guess operating system [ dronnikov@gmail.com ]
-	var captures;
-	for (var i = 0, len = operatingSystems.length; i < len; i += 1) {
-		var checkOs = operatingSystems[i];
-		if (captures = checkOs.regexp.exec(ua)) {
-			ua_obj.os = checkOs.os.indexOf('$1') === -1
-				? checkOs.os
-				: checkOs.os.replace(/\$(\d+)/g, function(_, n){return captures[n]});
+	// based on operating system guessing from [ dronnikov@gmail.com ]
+	// rewritten to use UA style parsing
+	for( i = 0, length = operatingSystems.length; i < length; i++ ){
+		os = parse( operatingSystems[i] );
+		if( os.match ){
+			ua_obj.os = os;
 			break;
 		}
 	}
@@ -127,178 +130,179 @@ exports.parser = function parser( ua, js_ua ){
 		os: ua_obj.os,
 
 		// return methods that can be used
-		pretty: pretty
+		pretty: pretty,
+		prettyOs: function( os ){ return pretty( os || ua_obj.os ) }
 	}
 };
 
 // doing a .join("|") because its easier to maintain as array,
 // if you want raw perfomance, compile this a string. 
 var browser_slash_v123_names = [
-		'Jasmine',
-		'ANTGalio',
-		'Midori',
-		'Fresco',
-		'Lobo',
-		'Maxthon',
-		'Lynx',
-		'OmniWeb',
-		'Dillo',
-		'Camino',
-		'Demeter',
-		'Fluid',
-		'Fennec',
-		'Shiira',
-		'Sunrise',
-		'Chrome',
-		'Flock',
-		'Netscape',
-		'Lunascape',
-		'Epiphany',
-		'WebPilot',
-		'Vodafone',
-		'NetFront',
-		'Konqueror',
-		'SeaMonkey',
-		'Kazehakase',
-		'Vienna',
-		'Iceape',
-		'Iceweasel',
-		'IceWeasel',
-		'Iron',
-		'K-Meleon',
-		'Sleipnir',
-		'Galeon',
-		'GranParadiso',
-		'Opera Mini',
-		'iCab',
-		'NetNewsWire',
-		'Iron',
-		'Iris',
-		'SeaRequin'
+		"Jasmine",
+		"ANTGalio",
+		"Midori",
+		"Fresco",
+		"Lobo",
+		"Maxthon",
+		"Lynx",
+		"OmniWeb",
+		"Dillo",
+		"Camino",
+		"Demeter",
+		"Fluid",
+		"Fennec",
+		"Shiira",
+		"Sunrise",
+		"Chrome",
+		"Flock",
+		"Netscape",
+		"Lunascape",
+		"Epiphany",
+		"WebPilot",
+		"Vodafone",
+		"NetFront",
+		"Konqueror",
+		"SeaMonkey",
+		"Kazehakase",
+		"Vienna",
+		"Iceape",
+		"Iceweasel",
+		"IceWeasel",
+		"Iron",
+		"K-Meleon",
+		"Sleipnir",
+		"Galeon",
+		"GranParadiso",
+		"Opera Mini",
+		"iCab",
+		"NetNewsWire",
+		"Iron",
+		"Iris",
+		"SeaRequin"
 	].join( "|" ),
 	
 	browser_slash_v12_names = [
-		'Bolt',
-		'Jasmine',
-		'Maxthon',
-		'Lynx',
-		'Arora',
-		'IBrowse',
-		'Dillo',
-		'Camino',
-		'Shiira',
-		'Fennec',
-		'Phoenix',
-		'Chrome',
-		'Flock',
-		'Netscape',
-		'Lunascape',
-		'Epiphany',
-		'WebPilot',
-		'Opera Mini',
-		'Opera',
-		'Vodafone',
-		'NetFront',
-		'Konqueror',
-		'SeaMonkey',
-		'Kazehakase',
-		'Vienna',
-		'Iceape',
-		'Iceweasel',
-		'IceWeasel',
-		'Iron',
-		'K-Meleon',
-		'Sleipnir',
-		'Galeon',
-		'GranParadiso',
-		'iCab',
-		'NetNewsWire',
-		'Iron',
-		'Space Bison',
-		'Stainless',
-		'Orca'
+		"Bolt",
+		"Jasmine",
+		"Maxthon",
+		"Lynx",
+		"Arora",
+		"IBrowse",
+		"Dillo",
+		"Camino",
+		"Shiira",
+		"Fennec",
+		"Phoenix",
+		"Chrome",
+		"Flock",
+		"Netscape",
+		"Lunascape",
+		"Epiphany",
+		"WebPilot",
+		"Opera Mini",
+		"Opera",
+		"Vodafone",
+		"NetFront",
+		"Konqueror",
+		"SeaMonkey",
+		"Kazehakase",
+		"Vienna",
+		"Iceape",
+		"Iceweasel",
+		"IceWeasel",
+		"Iron",
+		"K-Meleon",
+		"Sleipnir",
+		"Galeon",
+		"GranParadiso",
+		"iCab",
+		"NetNewsWire",
+		"Iron",
+		"Space Bison",
+		"Stainless",
+		"Orca"
 	].join( "|" ),
 	
 	// the parser regexp storage, this will be used by the UA parser
 	browsers = [
 		// TOP CASES
 		// opera must go first
-		{ regexp: /^(Opera)\/(\d+)\.(\d+) \(Nintendo Wii/, family_replacement:'Wii' },
+		{ regexp: /^(Opera)\/(\d+)\.(\d+) \(Nintendo Wii/, family_replacement:"Wii" },
 		
 		// must go before browser v1.v2
 		// eg: Minefield/3.1a1pre
-		{ regexp:/(Namoroka|Shiretoko|Minefield)\/(\d+)\.(\d+)\.(\d+(?:pre)?)/, family_replacement:'Firefox ($1)' },
-		{ regexp:/(Namoroka|Shiretoko|Minefield)\/(\d+)\.(\d+)([ab]\d+[a-z]*)?/, family_replacement:'Firefox ($1)' },
+		{ regexp:/(Namoroka|Shiretoko|Minefield)\/(\d+)\.(\d+)\.(\d+(?:pre)?)/, family_replacement:"Firefox ($1)" },
+		{ regexp:/(Namoroka|Shiretoko|Minefield)\/(\d+)\.(\d+)([ab]\d+[a-z]*)?/, family_replacement:"Firefox ($1)" },
 		{ regexp:/(SeaMonkey|Fennec|Camino)\/(\d+)\.(\d+)([ab]?\d+[a-z]*)/ },
 		// eg: Flock/2.0b2
 		{ regexp:/(Flock)\/(\d+)\.(\d+)(b\d+?)/ },
 		// eg: Fennec/0.9pre
 		{ regexp:/(Fennec)\/(\d+)\.(\d+)(pre)/ },
-		{ regexp:/(Navigator)\/(\d+)\.(\d+)\.(\d+)/, family_replacement:'Netscape' },
-		{ regexp:/(Navigator)\/(\d+)\.(\d+)([ab]\d+)/, family_replacement:'Netscape' },
-		{ regexp:/(Netscape6)\/(\d+)\.(\d+)\.(\d+)/, family_replacement:'Netscape' },
-		{ regexp:/(MyIBrow)\/(\d+)\.(\d+)/, family_replacement:'My Internet Browser' },
-		{ regexp:/(Firefox).*Tablet browser (\d+)\.(\d+)\.(\d+)/, family_replacement:'MicroB' },
+		{ regexp:/(Navigator)\/(\d+)\.(\d+)\.(\d+)/, family_replacement:"Netscape" },
+		{ regexp:/(Navigator)\/(\d+)\.(\d+)([ab]\d+)/, family_replacement:"Netscape" },
+		{ regexp:/(Netscape6)\/(\d+)\.(\d+)\.(\d+)/, family_replacement:"Netscape" },
+		{ regexp:/(MyIBrow)\/(\d+)\.(\d+)/, family_replacement:"My Internet Browser" },
+		{ regexp:/(Firefox).*Tablet browser (\d+)\.(\d+)\.(\d+)/, family_replacement:"MicroB" },
 		// Opera will stop at 9.80 and hide the real version in the Version string
 		// http://dev.opera.com/articles/view/opera-ua-string-changes/
 		{ regexp:/(Fennec)\/(\d+)\.(\d+)(pre)/ },
 		
-		{ regexp:/(Firefox)\/(\d+)\.(\d+)\.(\d+(?:pre)?) \(Swiftfox\)/, family_replacement:'Swiftfox' },
-		{ regexp:/(Firefox)\/(\d+)\.(\d+)([ab]\d+[a-z]*)? \(Swiftfox\)/, family_replacement:'Swiftfox' },
+		{ regexp:/(Firefox)\/(\d+)\.(\d+)\.(\d+(?:pre)?) \(Swiftfox\)/, family_replacement:"Swiftfox" },
+		{ regexp:/(Firefox)\/(\d+)\.(\d+)([ab]\d+[a-z]*)? \(Swiftfox\)/, family_replacement:"Swiftfox" },
 		
 		// lowercase konqueror
-		{ regexp:/(konqueror)\/(\d+)\.(\d+)\.(\d+)/, family_replacement:'Konqueror' },
+		{ regexp:/(konqueror)\/(\d+)\.(\d+)\.(\d+)/, family_replacement:"Konqueror" },
 		
 		
 		// MAIN CASES, this will match about 50% of the browsers
 		// browsers with v123
-		{ regexp:new RegExp( '(' + browser_slash_v123_names + ')\\/(\\d+)\\.(\\d+)\\.(\\d+)' ) },
+		{ regexp:new RegExp( "(" + browser_slash_v123_names + ")\\/(\\d+)\\.(\\d+)\\.(\\d+)" ) },
 		// browser with v12
-		{ regexp:new RegExp( '(' + browser_slash_v12_names +')\\/(\\d+)\\.(\\d+)' ) },
+		{ regexp:new RegExp( "(" + browser_slash_v12_names +")\\/(\\d+)\\.(\\d+)" ) },
 		// browser v123, space instead of slash
 		{ regexp:/(iRider|Crazy Browser|SkipStone|iCab|Lunascape|Sleipnir|Maemo Browser) (\d+)\.(\d+)\.(\d+)/ },
 		// browser v12, space instead of slash
 		{ regexp:/(iCab|Lunascape|Opera|Android) (\d+)\.(\d+)/ },
-		{ regexp:/(IEMobile) (\d+)\.(\d+)/, family_replacement:'IE Mobile' },
+		{ regexp:/(IEMobile) (\d+)\.(\d+)/, family_replacement:"IE Mobile" },
 		// do these after the "edge" cases
 		{ regexp:/(Firefox)\/(\d+)\.(\d+)\.(\d+)/ },
 		{ regexp:/(Firefox)\/(\d+)\.(\d+)(pre|[ab]\d+[a-z]*)?/ },
 		
 		// SPECIAL CASES
-		{ regexp:/(Obigo|OBIGO)[^\d]*(\d+)(?:.(\d+))?/, family_replacement:'Obigo' },
-		{ regexp:/(MAXTHON|Maxthon) (\d+)\.(\d+)/, family_replacement:'Maxthon' },
-		{ regexp:/(Maxthon|MyIE2|Uzbl|Shiira)/, V1_replacement:'0' },
-		{ regexp:/(PLAYSTATION) (\d+)/, family_replacement:'PlayStation' },
+		{ regexp:/(Obigo|OBIGO)[^\d]*(\d+)(?:.(\d+))?/, family_replacement:"Obigo" },
+		{ regexp:/(MAXTHON|Maxthon) (\d+)\.(\d+)/, family_replacement:"Maxthon" },
+		{ regexp:/(Maxthon|MyIE2|Uzbl|Shiira)/, V1_replacement:"0" },
+		{ regexp:/(PLAYSTATION) (\d+)/, family_replacement:"PlayStation" },
 		{ regexp:/(PlayStation Portable)[^\d]+(\d+).(\d+)/ },
 		{ regexp:/(BrowseX) \((\d+)\.(\d+)\.(\d+)/ },
-		{ regexp:/(Opera)\/(\d+)\.(\d+).*Opera Mobi/, family_replacement:'Opera Mobile' },
-		{ regexp:/(POLARIS)\/(\d+)\.(\d+)/, family_replacement:'Polaris' },
-		{ regexp:/(BonEcho)\/(\d+)\.(\d+)\.(\d+)/, family_replacement:'Bon Echo' },
+		{ regexp:/(Opera)\/(\d+)\.(\d+).*Opera Mobi/, family_replacement:"Opera Mobile" },
+		{ regexp:/(POLARIS)\/(\d+)\.(\d+)/, family_replacement:"Polaris" },
+		{ regexp:/(BonEcho)\/(\d+)\.(\d+)\.(\d+)/, family_replacement:"Bon Echo" },
 		{ regexp:/(iPod|iPhone|iPad)(?:[a-zA-Z;\s])* OS (\d+)_(\d+)(?:_(\d+))?/ },
-		{ regexp:/(Avant)/, V1_replacement:'1' },
+		{ regexp:/(Avant)/, V1_replacement:"1" },
 		{ regexp:/(Nokia)[EN]?(\d+)/ },
-		{ regexp:/(Black[bB]erry)(\d+)/, family_replacement:'Blackberry' },
+		{ regexp:/(Black[bB]erry)(\d+)/, family_replacement:"Blackberry" },
 		{ regexp:/(OmniWeb)\/v(\d+)\.(\d+)/ },
-		{ regexp:/(Blazer)\/(\d+)\.(\d+)/, family_replacement:'Palm Blazer' },
-		{ regexp:/(Pre)\/(\d+)\.(\d+)/, family_replacement:'Palm Pre' },
+		{ regexp:/(Blazer)\/(\d+)\.(\d+)/, family_replacement:"Palm Blazer" },
+		{ regexp:/(Pre)\/(\d+)\.(\d+)/, family_replacement:"Palm Pre" },
 		{ regexp:/(Links) \((\d+)\.(\d+)/ },
 		{ regexp:/(QtWeb) Internet Browser\/(\d+)\.(\d+)/ },
-		{ regexp:/(Version)\/(\d+)\.(\d+)(?:\.(\d+))?.*Safari\//, family_replacement:'Safari' },
+		{ regexp:/(Version)\/(\d+)\.(\d+)(?:\.(\d+))?.*Safari\//, family_replacement:"Safari" },
 		{ regexp:/(OLPC)\/Update(\d+)\.(\d+)/ },
-		{ regexp:/(OLPC)\/Update()\.(\d+)/, V1_replacement:'0' },
-		{ regexp:/(SamsungSGHi560)/, family_replacement:'Samsung SGHi560' },
-		{ regexp:/^(SonyEricssonK800i)/, family_replacement:'Sony Ericsson K800i' },
+		{ regexp:/(OLPC)\/Update()\.(\d+)/, V1_replacement:"0" },
+		{ regexp:/(SamsungSGHi560)/, family_replacement:"Samsung SGHi560" },
+		{ regexp:/^(SonyEricssonK800i)/, family_replacement:"Sony Ericsson K800i" },
 		{ regexp:/(Teleca Q7)/ },
-		{ regexp:/(MSIE) (\d+)\.(\d+)/, family_replacement:'Internet Explorer' }
+		{ regexp:/(MSIE) (\d+)\.(\d+)/, family_replacement:"Internet Explorer" }
 	],
 
 	operatingSystems = [
-		{ regexp: /ipad/i, os: 'iPad' },
-		{ regexp: /windows nt 6\.0/i, os: 'Windows Vista' },
-		{ regexp: /windows nt 6\.\d+/i, os: 'Windows 7' },
-		{ regexp: /windows nt 5\.2+/i, os: 'Windows 2003'},
-		{ regexp: /windows nt 5\.1+/i, os: 'Windows XP'},
-		{ regexp: /windows nt 5\.0+/i, os: 'Windows 2000'},
-		{ regexp: /os x (\d+)[._](\d+)/i, os: 'OS X $1.$2'},
-		{ regexp: /linux/i, os: 'Linux'}
+		{ regexp: /(iPod|iPhone|iPad)* OS (\d+)_(\d+)(?:_(\d+))?/, family_replacement:"iOS" },
+		{ regexp: /(windows) nt 6\.0/i, family_replacement:"Windows", V1_replacement:"Vista" },
+		{ regexp: /(windows) nt 6\.\d+/i, family_replacement:"Windows", V1_replacement:"7" },
+		{ regexp: /(windows) nt 5\.2+/i, family_replacement:"Windows", V1_replacement:"2003"},
+		{ regexp: /(windows) nt 5\.1+/i, family_replacement:"Windows", V1_replacement:"XP"},
+		{ regexp: /(windows) nt 5\.0+/i, family_replacement:"Windows", V1_replacement:"2000"},
+		{ regexp: /(os x) (\d+)[._](\d+)(?:[._](\d+))/i, family_replacement:"OS X"},
+		{ regexp: /(linux)/i, family_replacement:"Linux"}
 	];
