a26987d0f0f2b7d83e896589a17ea14a0417c406
Fintan Fairmichael <fintanf@gmail.com>
2011-05-10 13:23:33 +0100

    Fixing test suites and adding additional testing to the browser check to ensure no values in the returned object are undefined.

--- tests/browser.js
+++ tests/browser.js
@@ -3,18 +3,29 @@ var fs = require('fs'),
 	useragent = require('../useragent');
 
 fs.readFile( './useragents/user_agent_data.csv', function( err, result ){
-	
+
 	if( err )
 		return sys.puts( "can't locate file" );
 
 	// split file in single rows
-	var results = result.split( "\n" ), i = results.length,
+	var results = result.toString().split( "\n" ), i = results.length,
 			// start times
 		total, start = ( new Date() ).valueOf(), failed = [];
 
 	while( i-- ){
 		(function( ua, res ){
 			var agent = useragent.browser( ua.toString().replace( /\"/g, "" ) );
+
+			//Check all values are filled in correctly
+			for (var key in agent) {
+				if (agent[ key ] == undefined) {
+					failed[ failed.length ] = Array.prototype.concat.call( arguments, [] ).join(",");
+					sys.puts( JSON.stringify( agent ) );
+					sys.puts( "Undefined: " + key);
+					sys.puts( ua  + "\n");
+					break;
+				}
+			}
 
 			/*if( !agent || !agent.pretty() || !agent.match ){
 				failed[ failed.length ] = Array.prototype.concat.call( arguments, [] ).join(",");
@@ -26,12 +37,12 @@ fs.readFile( './useragents/user_agent_data.csv', function( err, result ){
 		}( results[i].split( "," ) ))
 	};
 
-	// report 
+	// report
 	if( failed.length ){
 		sys.puts( "Failed " + failed.length + " out of " + results.length + " tests." );
 	} else {
 		sys.puts( "\\o/ yay, passed all " + results.length + " tests \\o/" );
 	}
-	
+
 	sys.puts( "Executed in " + (( new Date()).valueOf() - start) + " ms");
 });
