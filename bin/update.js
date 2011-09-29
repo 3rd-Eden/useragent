43f5c5fa498e85fe5a73bb25310e8f01723f75d7
Arnout Kazemier <info@3rd-Eden.com>
2011-09-29 11:16:50 +0200

    spell check

--- bin/update.js
+++ bin/update.js
@@ -2,8 +2,8 @@
 
 var updater = require('../lib/update');
 updater(function (err, data) {
-  if (err)
-    return console.error('Update unsuccessfull due to errors', err.message);
+  if (err) return console.error('Update unsuccessfull due to errors', err.message);
 
-  console.log('successfully updated to the parsers');
+  // yay no issues
+  console.log('Successfully fetched and generated new parsers from the internets.');
 });
