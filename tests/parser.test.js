74c9d459ba9ee4df83e6edd130e507d7b78ea8d9
Arnout Kazemier <info@3rd-Eden.com>
2011-09-29 12:54:34 +0200

    Added support for `is`

--- tests/parser.test.js
+++ tests/parser.test.js
@@ -29,17 +29,29 @@ module.exports = {
   },
 
  'correctly parse chromes user agent': function () {
-  var agent = useragent.parse(ua);
+   var agent = useragent.parse(ua);
 
-  agent.family.should.equal('Chrome');
-  agent.major.should.equal('15');
-  agent.minor.should.equal('0');
-  agent.patch.should.equal('874');
+   agent.family.should.equal('Chrome');
+   agent.major.should.equal('15');
+   agent.minor.should.equal('0');
+   agent.patch.should.equal('874');
 
-  agent.os.should.equal('Mac OS X');
-  agent.toVersion().should.equal('15.0.874');
-  agent.toString().should.equal('Chrome 15.0.874 / Mac OS X');
-  agent.toAgent().should.equal('Chrome 15.0.874');
-  agent.toJSON().should.equal('{"family":"Chrome","major":"15","minor":"0","patch":"874","os":"Mac OS X"}');
+   agent.os.should.equal('Mac OS X');
+   agent.toVersion().should.equal('15.0.874');
+   agent.toString().should.equal('Chrome 15.0.874 / Mac OS X');
+   agent.toAgent().should.equal('Chrome 15.0.874');
+   agent.toJSON().should.equal('{"family":"Chrome","major":"15","minor":"0","patch":"874","os":"Mac OS X"}');
+ },
+
+ 'satisfy that range selector': function () {
+   var agent = useragent.parse(ua);
+
+   agent.satisfies('15.x || >=19.5.0 || 25.0.0 - 17.2.3').should.be_true;
+   agent.satisfies('>16.12.0').should.be_false;
+ },
+
+ 'simple detections': function () {
+   useragent.is(ua).chrome.should.be_true;
+   useragent.is(ua).webkit.should.be_true;
  }
 }
