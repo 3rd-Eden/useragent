803abd961845f61e71a8d19df1bf2e2c5c646d5d
Arnout Kazemier <info@3rd-Eden.com>
2012-04-02 10:37:27 +0200

    v1.0.6

--- bin/update.js
+++ bin/update.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
 var updater = require('../lib/update');
-updater(function (err, data) {
+updater(function updating (err, data) {
+  "use strict";
+
   if (err) return console.error('Update unsuccessfull due to errors', err.message);
 
   // yay no issues
