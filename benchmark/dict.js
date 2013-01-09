ff74860cf549b222e33bfa87387425a0d5c00d40
3rd-Eden <info@3rd-Eden.com>
2013-01-09 22:11:48 +0100

    [minor] Better output

--- benchmark/dict.js
+++ benchmark/dict.js
@@ -12,8 +12,7 @@ var microtime = require('microtime')
 /**
  * Useragent parsers
  */
-var useragent2 = require('../')
-  , useragent = require('useragent')
+var useragent = require('../')
   , uaparser = require('ua-parser')
   , useragent_parser = require('useragent_parser')
   , useragent_parser2 = require('useragent-parser');
@@ -35,19 +34,14 @@ var useragentlist = path.join(__dirname, '..', 'tests', 'fixtures', 'testcases.y
 var froomfroom = new benchmark.Suite;
 
 froomfroom
-.add('useragent latest', function () {
+.add('useragent', function () {
   for (var i = 0; i < length; i++ ) {
-    useragent2.parse(testcases[i]);
+    useragent.parse(testcases[i]);
   }
 })
-.add('useragent latest dictionary', function () {
+.add('useragent.lookup', function () {
   for (var i = 0; i < length; i++ ) {
-    useragent2.lookup(testcases[i]);
-  }
-})
-.add('useragent1', function () {
-  for (var i = 0; i < length; i++ ) {
-    useragent.parser(testcases[i]);
+    useragent.lookup(testcases[i]);
   }
 })
 .add('useragent_parser', function () {
@@ -68,7 +62,7 @@ froomfroom
 .on('cycle', function (event) {
   var details = event.target;
 
-  console.log('Executed benchmark (%s)', details.name);
+  console.log('Executed benchmark against node module: "%s"', details.name);
   console.log('Count (%d), Cycles (%d), Elapsed (%d), Hz (%d)\n'
    , details.count
    , details.cycles
@@ -77,11 +71,13 @@ froomfroom
   );
 })
 .on('complete', function () {
-  console.log(this.filter('fastest').pluck('name') + ' is the fastest parser');
+  console.log('Module: "'+ this.filter('fastest').pluck('name') +'" is the user agent fastest parser.');
 });
 
 /**
  * Start the benchmark, froom frooom!!
  */
 console.log('Starting the benchmark, parsing ' + length + ' useragent strings per run');
+console.log();
+
 froomfroom.run();
