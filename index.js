64b15c9446a24abd9f52ed4ceb970f1a5cf790dd
Matteo Collina <hello@matteocollina.com>
2017-02-06 14:57:52 +0100

    protect the parsing logic from malicious user-agents

--- index.js
+++ index.js
@@ -405,6 +405,28 @@ exports.Device = Device;
 exports.OperatingSystem = OperatingSystem;
 exports.Agent = Agent;
 
+function isSafe(userAgent) {
+  var consecutive = 0
+    , code = 0;
+
+  for (var i = 0; i < userAgent.length; i++) {
+    code = userAgent.charCodeAt(i);
+    // numbers between 0 and 9
+    if (code >= 48 && code <= 57) {
+      consecutive++;
+    } else {
+      consecutive = 0;
+    }
+
+    if (consecutive >= 100) {
+      return false;
+    }
+  }
+
+  return true
+}
+
+
 /**
  * Parses the user agent string with the generated parsers from the
  * ua-parser project on google code.
@@ -415,7 +437,7 @@ exports.Agent = Agent;
  * @api public
  */
 exports.parse = function parse(userAgent, jsAgent) {
-  if (!userAgent) return new Agent();
+  if (!userAgent || !isSafe(userAgent)) return new Agent();
 
   var length = agentparserslength
     , parsers = agentparsers
