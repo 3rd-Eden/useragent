687afe420d592488957510989f2e35976e50aa33
Сковорода Никита Андреевич <chalkerx@gmail.com>
2018-03-05 14:48:18 +0300

    [fix] long whitespace is evil, don't allow it
    There are broken sequences like "([^smth])*\s+" or "[^smth]+ *".
    This prevents abusing those.

--- index.js
+++ index.js
@@ -417,8 +417,8 @@ function isSafe(userAgent) {
 
   for (var i = 0; i < userAgent.length; i++) {
     code = userAgent.charCodeAt(i);
-    // numbers between 0 and 9, letters between a and z
-    if ((code >= 48 && code <= 57) || (code >= 97 && code <= 122)) {
+    // numbers between 0 and 9, letters between a and z, spaces and control
+    if ((code >= 48 && code <= 57) || (code >= 97 && code <= 122) || code <= 32) {
       consecutive++;
     } else {
       consecutive = 0;
